97d4f9c4302234830f0d8ecc50b40b01c4576196
Bart Jacobs <60138978+Cloudsparkle@users.noreply.github.com>
2020-10-15 17:37:29 +0200

    Update PSHostInfo.ps1

--- PSHostInfo.ps1
+++ PSHostInfo.ps1
@@ -1,35 +1,62 @@
-﻿[void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+<#
+.SYNOPSIS
+	Present hostname and IP with notification icon
+.DESCRIPTION
+	This script will display a notification icon and will display hostname and ip address for support usage
+.PARAMETER <Parameter_Name>
+	None
+.INPUTS
+  None
+.OUTPUTS
+  None
+.NOTES
+  Version:        	1.0
+  Author:         	Bart Jacobs - @Cloudsparkle
+  Creation Date:  	15/10/2020
+  Purpose/Change: 	Present hostname and ip address
+	General credits:  Denniver Reining - https://bytecookie.wordpress.com/2011/12/28/gui-creation-with-powershell-part-2-the-notify-icon-or-how-to-make-your-own-hdd-health-monitor/
+ .EXAMPLE
+	None
+#>
 
-$currentDir = [System.AppDomain]::CurrentDomain.BaseDirectory.TrimEnd('\') 
-if ($currentDir -eq $PSHOME.TrimEnd('\')) 
-{     
-	$currentDir = $PSScriptRoot 
+#load the framework to display stuff
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+
+#Get the current working directory
+#Credits 1: https://www.alkanesolutions.co.uk/2019/10/10/obtaining-the-current-working-directory-using-powershell/ - Kae Travis
+#Credits 2: https://powershell.org/forums/topic/compile-ps1-to-exe/ - Dave Wyatt
+$currentDir = [System.AppDomain]::CurrentDomain.BaseDirectory.TrimEnd('\')
+if ($currentDir -eq $PSHOME.TrimEnd('\'))
+{
+	$currentDir = $PSScriptRoot
 }
 
+#Set path to display icon
 $IconOKPath = $currentDir + "\support.ico"
-$IconWarnPath = $currentDir + "\TWS_Caller_5.ico"
 
+#Initialize
 $form1 = New-Object System.Windows.Forms.form
 $NotifyIcon= New-Object System.Windows.Forms.NotifyIcon
 $ContextMenu = New-Object System.Windows.Forms.ContextMenu
 $MenuItem = New-Object System.Windows.Forms.MenuItem
 $TimerPC = New-Object System.Windows.Forms.Timer
 $iconOK = New-Object System.Drawing.Icon($IconOKPath)
-$iconWarn = New-Object System.Drawing.Icon($IconWarnPath)
- 
+
 $form1.ShowInTaskbar = $false
 $form1.WindowState = "minimized"
- 
+
 $NotifyIcon.Icon =  $iconOK
 $NotifyIcon.ContextMenu = $ContextMenu
 $NotifyIcon.contextMenu.MenuItems.AddRange($MenuItem)
 $NotifyIcon.Visible = $True
- 
-$TimerPC.Interval =  300000  # (5 min)
 
-$TimerPC.add_Tick({FNCheckComputer})
+#Set the refresh timer
+$TimerPC.Interval =  30000  # (30 sec)
+
+$TimerPC.add_Tick({CheckComputer})
 $TimerPC.start()
- 
+
+#Set up the right-click menu
 $MenuItem.Text = "Exit"
 $MenuItem.add_Click({
    $TimerPC.stop()
@@ -37,24 +64,22 @@ $MenuItem.add_Click({
    $form1.close()
 })
 
-function FNCheckComputer
-    {
+#The part that actually gets the information
+function CheckComputer
+{
     $computername = $Env:computername
-    
-    
+
     $Netinfo = Test-Connection $computername -count 1 | select Address,Ipv4Address
     $ipaddress = $netinfo.IPV4Address.IPAddressToString
-    #$Text = $computername
-    $Text = 
+
+    $Text =
 @"
 ${computername}
 ${ipAddress}
 "@
-    
+
     $NotifyIcon.Text = $Text
+}
 
-    $NotifyIcon.Icon = $iconOK
-    } 
-
-FNcheckComputer
+CheckComputer
 [void][System.Windows.Forms.Application]::Run($form1)
